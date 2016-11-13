08b8bd21df503033d8ad71bcc8e3520523ccd9ea
Tim <objelisks@gmail.com>
2016-11-13 22:21:08 +0000

    crossword botttt

--- 6_regex_crossword/bot.js
+++ 6_regex_crossword/bot.js
@@ -7,7 +7,7 @@
         genius api
 */
 const seedrandom = require('seedrandom');
-const _ = require('lodash');
+let _;
 const fs = require('fs');
 const Canvas = require('canvas');
 
@@ -34,12 +34,14 @@ const buildGrid = (day) => {
     // seed generation so that we can regenerate old puzzles for solution
     const daySeed = '' + day.getFullYear() + day.getMonth() + day.getDate();
     const rng = seedrandom(daySeed);
-    let sample = (arr) => {
-        return arr[Math.floor(rng()*arr.length)];
-    }
+    // recapture the Math.random
+    // (oh god this actually works)
+    Math.random = rng;
+    delete require.cache['loadash'];
+    _ = require('lodash');
 
-    //const dayIndex = day.getDay(); // 0 is sunday
-    const dayIndex = 0;
+    const dayIndex = day.getDay(); // 0 is sunday
+    //const dayIndex = 0;
     const gridSize = [9,3,4,5,6,7,8][dayIndex];
     const alphabet = _.slice('ABCDEFGHI'.split(''), 0, gridSize);
 
@@ -47,38 +49,49 @@ const buildGrid = (day) => {
     const getRow = (grid, n) => _.nth(grid, n);
     
     const paren = (s) => `(${s})`;
-    const uniq = (s) => _.uniq(s).join('');
     const rch = (n) => _.sampleSize(alphabet, n).join('');
     const reducers = [
-        [/\((.)\)\((.)\)/, (m, p1, p2) => `(${p1 + p2})`],
-        [/\((.)\)\((.)\)/, (m, p1, p2) => `[${_.sortedUniq([p1,p2].sort()).join('')}]+`]
+        [/\((..?)\)\((..?)\)/, (m, p1, p2) => `(${p1 + p2})`],
+        [/\((..?)\)\((..?)\)/, (m, p1, p2) => `[${_.sortedUniq([p1,p2].join('').split('').sort()).join('')}]${Math.random() > 0.5 ? '*' : '+'}`],
     ];
     const herrings = [
         [/\(([^|]+?)\)/, (m, p1) => {
-            let a = `|${rch(p1.length)}`;
-            let b = Math.random() > 0.7 ? `${a}|${rch(p1.length)}` : `${a}`;
+            const a = `|${rch(p1.length)}`;
+            const b = Math.random() > 0.8 ? `${a}|${rch(p1.length)}` : `${a}`;
             return `(${p1}${b})`;
+        }],
+        [/\[([^-\]]{3,}?)\]/, (m, p1) => {
+            const inside = _.sortedUniq(p1.split('').sort()).join('');
+            return `[${_.first(inside)}-${_.last(inside)}]`;
         }]
+    ];
+    const cleanup = [
+        [/\((.+)(:?\|\1)+\)/, (m, p1) => `${p1}`],
+        [/\((.)\)/g, (m, p1) => `${p1}`],
+        [/\[(.)\]/g, (m, p1) => `${p1}`],
     ];
     const smallify = (str) => {
         for(let i=0; i<3; i++) {
-            let order = _.shuffle(reducers);
+            const order = _.shuffle(reducers);
             _.each(order, (things) => {
                 str = str.replace(things[0], things[1]);
             });
         }
         for(let i=0; i<3; i++) {
-            let order = _.shuffle(herrings);
+            const order = _.shuffle(herrings);
             _.each(order, (things) => {
                 str = str.replace(things[0], things[1]);
             });
         }
+        _.each(cleanup, (things) => {
+            str = str.replace(things[0], things[1]);
+        });
         return str;
     }
 
     const grid = _.range(gridSize)
         .map(y => _.range(gridSize)
-            .map(x => sample(alphabet)));
+            .map(x => _.sample(alphabet)));
 
     const columnClues = _.range(gridSize)
         .map(x => getColumn(grid, x))
@@ -101,12 +114,12 @@ const drawGrid = (gridData, isSolution) => {
     const gridSquareSize = 32;
     const fontOffsetX = 16;
     const fontOffsetY = 24;
-    const margin = 200.5;
-    const canvas = new Canvas(512, 512);
+    const margin = 256.5;//200.5;
+    const canvas = new Canvas(768, 768);
     const ctx = canvas.getContext('2d');
     
     ctx.fillStyle = 'rgb(255, 255, 255)';
-    ctx.fillRect(0, 0, 512, 512);
+    ctx.fillRect(0, 0, 768, 768);
     
     ctx.font = '24px sans-serif';
     ctx.textAlign = 'center';
@@ -157,10 +170,50 @@ const todaysPuzzle = buildGrid(today);
 const solutionCanvas = drawGrid(yesterdaysPuzzle, true);
 const newCanvas = drawGrid(todaysPuzzle);
 
-const out = fs.createWriteStream('./out.png');
-solutionCanvas.pngStream().pipe(out);
+//const out = fs.createWriteStream('./out.png');
+//newCanvas.pngStream().pipe(out);
 
 // post tweet
+const Twitter = require('twitter');
+const client = new Twitter(JSON.parse(fs.readFileSync('./creds.json')));
+
+const days = [
+    "Sunday's",
+    "Monday's",
+    "Tuesday's",
+    "Wednesday's",
+    "Thursday's",
+    "Friday's",
+    "Saturday's"
+];
 
 // post solution to old puzzle
-// post new puzzle
+client.post('media/upload', {media: solutionCanvas.toBuffer()}, (error, media, res) => {
+    if(error) throw error;
+    
+    let status = {
+        status: "The solution to " + days[yesterday.getDay()] + " puzzle!",
+        media_ids: media.media_id_string
+    };
+
+    client.post('statuses/update', status, (err, tweet) => {
+        if(err) throw err;
+        console.log('yesterday tweet success');
+    });
+});
+
+setTimeout(() => {
+    // post new puzzle
+    client.post('media/upload', {media: newCanvas.toBuffer()}, (error, media, res) => {
+        if(error) throw error;
+        let status = {
+            status: "Here's " + days[today.getDay()] + " puzzle!",
+            media_ids: media.media_id_string
+        };
+    
+        client.post('statuses/update', status, (err, tweet) => {
+            if(err) throw err;
+            console.log('today tweet success');
+        });
+    });
+}, 5000);
